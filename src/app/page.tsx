f4dfd8c7a2c66e387c3d2de64e356b6ac390bda5
Trung Son <trungsonvonguyen@gmail.com>
2025-01-15 10:58:03 +0700

    Config Poppins Font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import HomePage from "@/components/layout/homepage";
-
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <HomePage />
+      <Button variant="gradientOrangeBtn">Sign up</Button>
     </div>
   );
 }
