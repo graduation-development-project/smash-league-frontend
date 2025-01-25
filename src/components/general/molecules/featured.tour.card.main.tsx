6da25133fddc9d24eb5ebcc2ec33f370e56ddb11
Trung Son <trungsonvonguyen@gmail.com>
2025-01-26 01:43:45 +0700

    Modify FeaturedTourCardMain by SliderImage

--- src/components/general/molecules/featured.tour.card.main.tsx
+++ src/components/general/molecules/featured.tour.card.main.tsx
@@ -1,5 +1,5 @@
-import images from "@/assets/images";
-import Image from "next/image";
+"use client";
+
 import React from "react";
 import {
   TrophyIcon,
@@ -9,46 +9,44 @@ import {
 } from "@/assets/icons";
 import { Button } from "../../ui/button";
 import TextGradientBtn from "../atoms/text.gradient.btn";
+import TourCardSliderMain from "../atoms/tour.card.slider.main";
+import { useHomeContext } from "@/library/home.context";
+import { sliderImages } from "@/assets/images";
 
 const FeaturedTourCardMain = () => {
+  const { activeSlide } = useHomeContext();
+  const slide = sliderImages.find((item) => item.id === activeSlide);
+
   return (
-    <div className="w-full h-full flex flex-col">
+    <div className="w-full flex flex-col">
       <div className="relative w-full h-full flex flex-col">
-        <div className="absolute w-full h-[95%] z-0 shadow-shadowComp rounded-[15px]">
-          <Image
-            src={images.featuredTourCardMain}
-            alt="FeaturedTournamentMain"
-            layout="fill"
-            objectFit="cover"
-            quality={100}
-            priority
-            className="rounded"
-          />
+        <div className="absolute w-full h-full z-0 shadow-shadowComp rounded-[15px]">
+          <TourCardSliderMain />
         </div>
         <div className="relative z-10 flex flex-col p-10 gap-3">
           <div className="flex flex-col gap-2">
             <h1 className="font-semibold text-[24px] text-white leading-[20px]">
-              All Star Season 2025
+              {slide?.title}
             </h1>
             <h4 className="text-[16px] text-white leading-[20px]">
-              The Shuttlecock Masters Championship
+              {slide?.description}
             </h4>
           </div>
           <div className="flex py-[5px] px-[15px] items-center rounded-[20px] gap-2 bg-[#FFFFFF] bg-opacity-30 w-max">
             <span className="font-bold text-[16px] text-white leading-[32px] flex justify-center items-center gap-2">
               <TrophyIcon />
-              Total Prize Pool: $50,000
+              Total Prize Pool: ${slide?.prize}
             </span>
           </div>
           <ul className="flex flex-col gap-2 text-[16px] text-white leading-[16px]">
             <li className="flex items-center gap-2">
-              <CanlendarIcon /> March 15-20, 2024
+              <CanlendarIcon /> {slide?.date}
             </li>
             <li className="flex items-center gap-2">
-              <ClockIcon /> Registration Deadline: March 1st, 2024
+              <ClockIcon /> Registration Deadline: {slide?.registerDeadline}
             </li>
             <li className="flex items-center gap-2">
-              <LocationIcon /> International Sports Arena, Singapore
+              <LocationIcon /> {slide?.location}
             </li>
           </ul>
           <div className="flex gap-5 mt-10">
---
+++ src/library/home.context.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { createContext, useContext, useState } from "react";
+
+interface TourCardSliderContextProps {
+  activeSlide: number;
+  setActiveSlide: (slide: number) => void;
+}
+
+const HomeContext = createContext<TourCardSliderContextProps | null>(null);
+
+export const HomeContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [activeSlide, setActiveSlide] = useState(1);
+
+  return (
+    <HomeContext.Provider value={{ activeSlide, setActiveSlide }}>
+      {children}
+    </HomeContext.Provider>
+  );
+};
+
+export const useHomeContext = () => {
+  const context = useContext(HomeContext);
+
+  if (!context) {
+    throw new Error("useHomeContext must be used within a HomeContextProvider");
+  }
+
+  return context;
+};
