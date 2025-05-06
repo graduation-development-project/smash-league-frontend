bca197f2c7faf3adf654483b63615d28e184824b
Trung Son <trungsonvonguyen@gmail.com>
2025-05-06 23:08:37 +0700

    fix all organizers, score, profile

--- src/components/general/atoms/organizers/organizers.zone.info.card.tsx
+++ src/components/general/atoms/organizers/organizers.zone.info.card.tsx
@@ -1,15 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
-"use client";
-import { Button } from "@/components/ui/button";
-import { Image } from "antd";
-import React from "react";
-import { GoPeople } from "react-icons/go";
-import { TbTournament } from "react-icons/tb";
-import { FaLocationDot } from "react-icons/fa6";
-import { useRouter } from "next/navigation";
-import { useProfileContext } from "@/context/profile.context";
+'use client';
+import { Button } from '@/components/ui/button';
+import { Image } from 'antd';
+import React from 'react';
+import { GoPeople } from 'react-icons/go';
+import { TbTournament } from 'react-icons/tb';
+import { FaLocationDot } from 'react-icons/fa6';
+import { useRouter } from 'next/navigation';
+import { useProfileContext } from '@/context/profile.context';
 
-const OrganizersZoneInfoCard = () => {
+const OrganizersZoneInfoCard = ({ organizer }: { organizer: any }) => {
   const router = useRouter();
   const { organizerId, setOrganizerId } = useProfileContext();
 
@@ -17,8 +17,8 @@ const OrganizersZoneInfoCard = () => {
     <div
       className="w-max h-max flex flex-col gap-3 p-5 rounded-[10px] bg-white border border-solid border-gray-400 transition-all ease-in-out cursor-pointer hover:shadow-shadowComp hover:border-none hover:outline-none"
       onClick={() => {
-        router.push(`/profile/organizer/organizer-name`);
-        setOrganizerId("organizer-id");
+        router.push(`/profile/organizer/${organizer?.name}`);
+        setOrganizerId(organizer?.id);
       }}
     >
       <div className="w-full">
@@ -31,22 +31,22 @@ const OrganizersZoneInfoCard = () => {
         />
       </div>
       <div className="w-full h-full flex flex-col gap-2">
-        <h2 className="text-[22px] font-bold ">Organizer Name</h2>
+        <h2 className="text-[22px] font-bold ">{organizer?.name}</h2>
         <p className=" items-center gap-1 text-[16px] hidden">
           <GoPeople /> 50 Followers
         </p>
-        <p className="flex items-center gap-1 text-[16px]">
+        <p className="flex items-center gap-1 text-[16px] italic">
           <TbTournament />
-          50 Tournaments
+          {organizer?.email}
         </p>
         <p className="flex items-center gap-1 text-[16px]">
           <FaLocationDot />
-          Thu Duc, Ho Chi Minh
+          {organizer?.location ? organizer?.location : 'No infomation'}
         </p>
       </div>
       <div className="w-full h-full flex justify-center items-center">
         <Button
-          size={"sm"}
+          size={'sm'}
           className="w-full"
           onClick={() => {
             router.push(`/profile/organizer/organizer-name`);
